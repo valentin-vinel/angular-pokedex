9c4770ab2e979a7ccc08a439c2dcad50fda54a96
valentin-vinel <valentinvnl84@gmail.com>
2025-07-30 11:48:45 +0200

    Composant

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`,
+  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`,
 }) 
-export class AppComponent {
-  title = 'ng-pokemon-app';
-} 
+export class AppComponent implements OnInit {
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit() {
+    console.table(this.pokemonList)
+    this.selectPokemon('Bulbizarre')
+  }
+
+  selectPokemon(pokemonName: string) {
+    console.log(`Vous avez cliqué sur le pokemon: ${pokemonName}`)
+  }
+}
