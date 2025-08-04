2e562ba0fcfb90e3abc61cafe6a098c8d731e5ae
valentin-vinel <valentinvnl84@gmail.com>
2025-08-04 10:24:45 +0200

    Requêtes HTTP: Create

--- ng-pokemon-app/src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ ng-pokemon-app/src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -12,6 +12,7 @@ export class PokemonFormComponent implements OnInit {
 
   @Input() pokemon: Pokemon;
   types: string[];
+  isAddForm: boolean;
 
   constructor(
     private pokemonService: PokemonService,
@@ -20,6 +21,7 @@ export class PokemonFormComponent implements OnInit {
 
   ngOnInit() {
     this.types = this.pokemonService.getPokemonTypeList();
+    this.isAddForm = this.router.url.includes('add');
   }
 
   hasType(type: string): boolean {
@@ -50,8 +52,13 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.pokemonService.updatePokemon(this.pokemon)
-      .subscribe(() => { this.router.navigate(['/pokemon', this.pokemon.id]) })
+    if(this.isAddForm) {
+      this.pokemonService.addPokemon(this.pokemon)
+        .subscribe((pokemon: Pokemon) => this.router.navigate(['/pokemon', pokemon.id]));
+    } else {
+      this.pokemonService.updatePokemon(this.pokemon)
+        .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]))
+    }
   }
 
 }
--- ng-pokemon-app/src/app/pokemon/pokemon.service.ts
+++ ng-pokemon-app/src/app/pokemon/pokemon.service.ts
@@ -22,6 +22,17 @@ export class PokemonService {
     )
   }
 
+  addPokemon(pokemon: Pokemon): Observable<Pokemon> {
+    const httpOptions= {
+      headers: new HttpHeaders({ 'Content-Type' : 'application/json' })
+    };
+
+    return this.http.post<Pokemon>('api/pokemons', pokemon, httpOptions).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, null))
+    )
+  }
+
   updatePokemon(pokemon: Pokemon): Observable<null> {
     const httpOptions= {
       headers: new HttpHeaders({ 'Content-Type' : 'application/json' })
